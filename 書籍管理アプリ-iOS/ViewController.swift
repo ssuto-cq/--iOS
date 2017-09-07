40a8a2bf35e7862e618a20021079c8292189c303
ssuto-cq <s.suto@caraquri.com>
2017-09-07 14:47:42 +0900

    Initial Commit

---
+++ 書籍管理アプリ-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  書籍管理アプリ-iOS
+//
+//  Created by 須藤 翔太 on 2017/09/07.
+//  Copyright © 2017年 須藤 翔太. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
