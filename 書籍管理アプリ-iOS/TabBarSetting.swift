45d31add9f5158e1b28179554fc0c7db63005dd7
ssuto-cq <s.suto@caraquri.com>
2017-09-28 10:54:02 +0900

    インデントの修正

--- 書籍管理アプリ-iOS/TabBarSetting.swift
+++ 書籍管理アプリ-iOS/TabBarSetting.swift
@@ -1,27 +1,27 @@
 import UIKit
 
 class TabBarSetting: UITabBarController, UITabBarControllerDelegate {
-
-        override func viewDidLoad() {
+    
+    override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         var navigationController: [UIViewController] = []
-
+        
         //書籍一覧画面
         let booksViewController = UINavigationController(rootViewController: BooksViewController())
         let booksViewTitle = R.string.localizable.booksview()
         booksViewController.tabBarItem = UITabBarItem(title:booksViewTitle, image:nil, tag: 1)
         navigationController.append(booksViewController)//配列の要素を追加
-
+        
         //設定前画面
         let beforeSettingViewController = UINavigationController(rootViewController: BeforeSettingViewController())
         let settingTitle = R.string.localizable.setting()
         beforeSettingViewController.tabBarItem = UITabBarItem(title: settingTitle, image:nil, tag: 2)
         navigationController.append(beforeSettingViewController)//配列の要素を追加
-
+        
         delegate = self
         viewControllers = navigationController
-
+        
     }
-
+    
 }
